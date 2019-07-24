4c4b4d84384160677b85c5bb5cd8cddfd90498cf
JackShiqiLi <u6123323@anu.edu.au>
2019-07-23 22:51:25 -0500

    Some attempts on openmp and MPI homeworks.

--- ProblemSets/day3/Bcast/broadcast.cpp
+++ ProblemSets/day3/Bcast/broadcast.cpp
@@ -3,17 +3,18 @@
 
 int main(int argc, char *argv[])
 {
-    int rank, data;
+    int rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank==0) {
-        printf("enter a value:\n");
-        fflush(stdout);
-        scanf ("%d",&data);
+        int data = 2;
+    }
+    else {
+      int data = 5;
     }
 
     /* broadcast the value of data of rank 0 to all ranks */
-    MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    //MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     printf("I am rank %i and the value is %i\n", rank, data);
     MPI_Finalize();
