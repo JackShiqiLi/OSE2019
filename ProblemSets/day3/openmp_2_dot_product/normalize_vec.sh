f7ace7538a4a4c46fc78c588ba4bc92f701fae9d
JackShiqiLi <u6123323@anu.edu.au>
2019-07-24 15:15:29 -0500

    a new sh file to run on cluster

---
+++ ProblemSets/day3/openmp_2_dot_product/normalize_vec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+
+#SBATCH --time=00:01:00
+
+
+#SBATCH --job-name=dot_prod
+#SBATCH --output=dot_prod.out
+
+export OMP_NUM_THREADS=20
+
+
+### openmp executable
+./dot_prod.exec
