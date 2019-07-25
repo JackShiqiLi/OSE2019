147457f697e211951874547f7e3df2547ef18610
JackShiqiLi <u6123323@anu.edu.au>
2019-07-25 16:54:27 -0500

    some local changes during cluster trials.

--- ProblemSets/day3/pi_monte_carlo/pi_MC_omp.cpp
+++ ProblemSets/day3/pi_monte_carlo/pi_MC_omp.cpp
@@ -3,24 +3,10 @@
 #include <ctime>        //defines ctime() for random numbers
 #include <cmath>        //defines math functions
 #include <omp.h>
-using namespace std;
 
-//calculates the distance of the dart from the origin
-double dist(double x, double y)
-{
-    double throwDist;
-    throwDist = sqrt(x*x + y*y);  //Euclidean Distance Formula from point (0,0)
-    return throwDist;
-}
-//calculates pi using number of darts thrown in the circle vs all darts thrown
-double pi(int numThrows, double a)
-{
-    double piValue = a / numThrows;
-    return piValue;
-}
 int main()
 {
-    double coord_x, coord_y, test;
+    double x, y, test;
     srand(time(NULL));      //creates the seed time
     test = rand() / double(RAND_MAX);
     double throwDist;       //distance from the origin
@@ -35,16 +21,16 @@ int main()
     #pragma omp for
     for(i = 1; i <= numThrows * 4; i++)
     {
-        coord_x = (double)rand() / (RAND_MAX);
-        coord_y = (double)rand() / (RAND_MAX);
-        throwDist = dist(coord_x, coord_y);
+        x = (double)rand() / (RAND_MAX);
+        y = (double)rand() / (RAND_MAX);
+        throwDist = sqrt(x*x + y*y);
         //counts how often the dart is thrown inside the circle
         if(throwDist <= 1)
         {
             a++;
         }
     }
-    double pi_value = pi(numThrows, a);
+    double pi_value = a / numThrows;
     cout<< "The value of PI is: " << pi_value;
 return 0;
 }
