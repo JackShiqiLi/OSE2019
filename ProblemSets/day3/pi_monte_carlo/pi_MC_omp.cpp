c6165565b93dd465200a66fe139c594494eea977
rccguest 9465 <rccguest9465@midway2-login1.rcc.local>
2019-07-25 16:52:10 -0500

    Trials for pi_MC_omp, parallel seems to work, but parallel codes takes more time than using 1 threads, will take more looks later.

--- ProblemSets/day3/pi_monte_carlo/pi_MC_omp.cpp
+++ ProblemSets/day3/pi_monte_carlo/pi_MC_omp.cpp
@@ -1,50 +1,44 @@
 #include <iostream>
+#include <vector>
 #include <cstdlib>      //defines rand(), srand(), RAND_MAX
 #include <ctime>        //defines ctime() for random numbers
 #include <cmath>        //defines math functions
 #include <omp.h>
-using namespace std;
 
-//calculates the distance of the dart from the origin
-double dist(double x, double y)
-{
-    double throwDist;
-    throwDist = sqrt(x*x + y*y);  //Euclidean Distance Formula from point (0,0)
-    return throwDist;
-}
-//calculates pi using number of darts thrown in the circle vs all darts thrown
-double pi(int numThrows, double a)
-{
-    double piValue = a / numThrows;
-    return piValue;
-}
 int main()
 {
-    double coord_x, coord_y, test;
+    double x, y;
     srand(time(NULL));      //creates the seed time
-    test = rand() / double(RAND_MAX);
-    double throwDist;       //distance from the origin
     double a = 0;              //times a dart ends up inside the circle
-    int numThrows;          //times to throw the dart
+    int numThrows = 5000000;          //times to throw the dart
+    std::vector<double> throwDist(numThrows * 4);
     int num_threads = omp_get_max_threads();
-    int i
-    numThrows = 1000000;
-
+    int i;
+    double time_parallel = -omp_get_wtime();
+   
     //creates 2 random numbers 'n' times
-    #pragma omp parallel shared(numThrows, a) private(throwDist, i)
+    #pragma omp parallel shared(a) private( i)
     #pragma omp for
-    for(i = 1; i <= numThrows * 4; i++)
+    for(i = 0; i <= numThrows; i++)
     {
-        coord_x = (double)rand() / (RAND_MAX);
-        coord_y = (double)rand() / (RAND_MAX);
-        throwDist = dist(coord_x, coord_y);
-        //counts how often the dart is thrown inside the circle
-        if(throwDist <= 1)
+        x = (double)rand() / (RAND_MAX);
+        y = (double)rand() / (RAND_MAX);
+        throwDist[i] = sqrt(x*x + y*y);
+    }
+    //counts how often the dart is thrown inside the circle
+    #pragma omp parallel for reduction(+:a)
+    for(i = 0; i<= numThrows; i++)
+    {
+        if(throwDist[i] <= 1)
         {
-            a++;
+            a += 1;
         }
     }
-    double pi_value = pi(numThrows, a);
-    cout<< "The value of PI is: " << pi_value;
+    time_parallel += omp_get_wtime();
+    double pi_value = a / numThrows * 4;
+    std::cout << "Compute the value of PI = " << pi_value << std::endl;
+    std::cout << "parallely that took " << time_parallel << " seconds" << std::endl;
+
+    std::cout << "==============================================================" << '\n';
 return 0;
 }
