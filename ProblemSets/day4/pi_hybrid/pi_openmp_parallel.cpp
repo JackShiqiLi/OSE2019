326329eaf74684d45013df3b62a838e6c5329e1e
JackShiqiLi <u6123323@anu.edu.au>
2019-07-30 13:53:33 -0500

    homeworks arranged for submission

---
+++ ProblemSets/day4/pi_hybrid/pi_openmp_parallel.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include "mpi.h"
+#include <omp.h>
+
+#define _USE_MATH_DEFINES
+
+const int num_steps = 500000000;
+
+int main(int argc, char *argv[]){
+    int i, rank, numprocs;
+    double sum = 0.0;
+    double pi  = 0.0;
+
+    std::cout << "using " << omp_get_max_threads() << " OpenMP threads" << std::endl;
+
+    const double w = 1.0/double(num_steps);
+
+    double time = -omp_get_wtime();
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    #pragma omp parallel for reduction(+:sum)
+    for(int i=0; i<num_steps; ++i) {
+        double x = (i+0.5)*w;
+        sum += 4.0/(1.0+x*x);
+    }
+    
+    MPI_Finalize();
+    pi = sum*w;
+
+    time += omp_get_wtime();
+
+    std::cout << num_steps
+              << " steps approximates pi as : "
+              << pi
+              << ", with relative error "
+              << std::fabs(M_PI-pi)/M_PI
+              << std::endl;
+    std::cout << "the solution took " << time << " seconds" <<std::endl;
+}
